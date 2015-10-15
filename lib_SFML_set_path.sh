66ee1cf2a2423120564e2d5d6910f8e083a0a4da
tdieumeg <tdieumeg@student.42.fr>
2015-10-15 17:36:20 +0200

    lib...sh fix

--- lib_SFML_set_path.sh
+++ lib_SFML_set_path.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export LD_LIBRARY_PATH=/nfs/zfs-student-4/users/2013_paris/tdieumeg/Rendu/Nibbler_github/SFML/lib
+export LD_LIBRARY_PATH=$PWD/SFML/lib
 /bin/zsh
 v=$(expr $SHLVL - 1)
 echo "Shell lvl: $v"
