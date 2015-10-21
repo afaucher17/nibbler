49079e1f1d9dd489ba8f2e49e7aac979a9a6facb
tdieumeg <tdieumeg@student.42.fr>
2015-10-21 13:41:18 +0200

    lots of ncurses changes

--- lib_ncurses/includes/LibNcurses.hpp
+++ lib_ncurses/includes/LibNcurses.hpp
@@ -6,16 +6,17 @@
 /*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/19 12:01:04 by tdieumeg          #+#    #+#             */
-/*   Updated: 2015/10/19 17:03:17 by tdieumeg         ###   ########.fr       */
+/*   Updated: 2015/10/21 12:47:54 by tdieumeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIB_NCURSES_HPP
 # define LIB_NCURSES_HPP
 
+# include <ncurses.h>
 # include <map>
 # include <list>
-# include <ncurses.h>
+# include "IGraphicLib.interface.hpp"
 # include "IGameObject.interface.hpp"
 
 # define K_ESC		27
@@ -24,26 +25,28 @@
 # define K_LEFT		4479771
 # define K_RIGHT	4414235
 
-class LibNcurses {
-
+class LibNcurses : public IGraphicLib
+{
 	public:
-	LibNcurses(int height, int width, std::string winName);
-	LibNcurses(LibNcurses const & src);
-	virtual ~LibNcurses(void);
+		LibNcurses(int height, int width, std::string winName);
+		LibNcurses(LibNcurses const & src);
+		virtual ~LibNcurses(void);
 
-	LibNcurses			&operator=(LibNcurses const & rhs);
+		LibNcurses			&operator=(LibNcurses const & rhs);
 
-	int					keyhandler(void);
-	void				display(std::list<IGameObject*> const game_objects);
-	void				display_score(std::list<int> const scores);
+		int					keyhandler(void);
+		void				display(std::list<IGameObject*> const game_objects);
+		void				display_score(std::list<int> const scores);
 
-private:
-	LibNcurses(void);
-	void				_display_sprite(int const x, int const y, char const sprite, type_e const type);
-	void				_terminate();
+	private:
+		LibNcurses(void);
+		void				_display_sprite(int const x, int const y, std::string const sprite, type_e const type);
+		void				_draw_outline(void);
+		void				_terminate();
 
-	WINDOW				*_window;
-	static std::map<type_e, std::map<cardinal_e, char> >	_sprites;
+		WINDOW				*_window;
+		std::string			_name;
+		static std::map<type_e, std::map<cardinal_e, std::string> >	_sprites;
 };
 
 #endif
--- lib_ncurses/srcs/LibNcurses.cpp
+++ lib_ncurses/srcs/LibNcurses.cpp
@@ -6,49 +6,23 @@
 /*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/19 12:27:33 by tdieumeg          #+#    #+#             */
-/*   Updated: 2015/10/19 17:03:15 by tdieumeg         ###   ########.fr       */
+/*   Updated: 2015/10/21 13:36:47 by tdieumeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "LibNcurses.hpp"
 
-std::map<type_e, std::map<cardinal_e, char> >		LibNcurses::_sprites =
+std::map<type_e, std::map<cardinal_e, std::string> >		LibNcurses::_sprites =
 {
-	{SNAKE_HEAD_1,{ {NORTH, '^'} }},
-	{SNAKE_HEAD_1,{ {EAST, '>'} }},
-	{SNAKE_HEAD_1,{ {WEST, '<'} }},
-	{SNAKE_HEAD_1,{ {SOUTH, 'v'} }},
-	{SNAKE_BODY_1,{ {NORTH, 186} }},
-	{SNAKE_BODY_1,{ {EAST, 205} }},
-	{SNAKE_BODY_1,{ {WEST, 205} }},
-	{SNAKE_BODY_1,{ {SOUTH, 186} }},
-	{SNAKE_BODY_1,{ {NORTH_EAST, 187} }},
-	{SNAKE_BODY_1,{ {NORTH_WEST, 201} }},
-	{SNAKE_BODY_1,{ {SOUTH_EAST, 188} }},
-	{SNAKE_BODY_1,{ {SOUTH_WEST, 200} }},
-	{SNAKE_TAIL_1,{ {NORTH, 186} }},
-	{SNAKE_TAIL_1,{ {EAST, 205} }},
-	{SNAKE_TAIL_1,{ {WEST, 205} }},
-	{SNAKE_TAIL_1,{ {SOUTH, 186} }},
-	{SNAKE_HEAD_2,{ {NORTH, '^'} }},
-	{SNAKE_HEAD_2,{ {EAST, '>'} }},
-	{SNAKE_HEAD_2,{ {WEST, '<'} }},
-	{SNAKE_HEAD_2,{ {SOUTH, 'v'} }},
-	{SNAKE_BODY_2,{ {NORTH, 186} }},
-	{SNAKE_BODY_2,{ {EAST, 205} }},
-	{SNAKE_BODY_2,{ {WEST, 205} }},
-	{SNAKE_BODY_2,{ {SOUTH, 186} }},
-	{SNAKE_BODY_2,{ {NORTH_EAST, 187} }},
-	{SNAKE_BODY_2,{ {NORTH_WEST, 201} }},
-	{SNAKE_BODY_2,{ {SOUTH_EAST, 188} }},
-	{SNAKE_BODY_2,{ {SOUTH_WEST, 200} }},
-	{SNAKE_TAIL_2,{ {NORTH, 186} }},
-	{SNAKE_TAIL_2,{ {EAST, 205} }},
-	{SNAKE_TAIL_2,{ {WEST, 205} }},
-	{SNAKE_TAIL_2,{ {SOUTH, 186} }},
-	{OBSTACLE, { {NORTH, ' '} }},
-	{FOOD, { {NORTH, 'o'} }},
-	{UNKNOWN, { {NORTH, '?'} }}
+	{SNAKE_HEAD_1,{ {NORTH, "^"}, {EAST, ">"}, {WEST, "<"}, {SOUTH, "v"} }},
+	{SNAKE_BODY_1,{ {NORTH, "|"}, {EAST, "-"}, {WEST, "-"}, {SOUTH, "|"}, {NORTH_EAST, "+"}, {NORTH_WEST, "+"}, {SOUTH_EAST, "+"}, {SOUTH_WEST, "+"} }},
+	{SNAKE_TAIL_1,{ {NORTH, "|"}, {EAST, "-"}, {WEST, "-"}, {SOUTH, "|"} }},
+	{SNAKE_HEAD_2,{ {NORTH, "^"}, {EAST, ">"}, {WEST, "<"}, {SOUTH, "v"} }},
+	{SNAKE_BODY_2,{ {NORTH, "|"}, {EAST, "-"}, {WEST, "-"}, {SOUTH, "|"}, {NORTH_EAST, "+"}, {NORTH_WEST, "+"}, {SOUTH_EAST, "+"}, {SOUTH_WEST, "+"} }},
+	{SNAKE_TAIL_2,{ {NORTH, "|"}, {EAST, "-"}, {WEST, "-"}, {SOUTH, "|"} }},
+	{OBSTACLE, { {NORTH, " "} }},
+	{FOOD, { {NORTH, "o"} }},
+	{UNKNOWN, { {NORTH, "?"} }}
 };
 
 LibNcurses::LibNcurses(void)
@@ -86,11 +60,7 @@ LibNcurses::LibNcurses(int height, int width, std::string winName)
 	refresh();
 	curs_set(0);
 	this->_window = newwin(height, width, 0, 0);
-	wattron(this->_window, COLOR_PAIR(1));
-	wborder(this->_window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-	wattroff(this->_window, COLOR_PAIR(1));
-	wmove(this->_window, 0, ((width / 2) - (winName.length() / 2)));
-	wprintw(this->_window, "%s", winName.c_str());
+	this->_name = winName;
 	keypad(this->_window, true);
 	return ;
 }
@@ -114,16 +84,22 @@ int					LibNcurses::keyhandler(void)
 
 void				LibNcurses::display(std::list<IGameObject*> const game_objects)
 {
-	std::list<IGameObject*>::const_iterator		obj;
-	std::map<type_e, std::map<cardinal_e, char> >::iterator		type;
-	std::map<cardinal_e, char>::iterator	dir;
+	std::list<IGameObject*>::const_iterator								obj;
+	std::map<type_e, std::map<cardinal_e, std::string> >::iterator		type;
+	std::map<cardinal_e, std::string>::iterator							dir;
 
 	werase(this->_window);
 	for (obj = game_objects.begin(); obj != game_objects.end(); obj++)
-		for (type = LibNcurses::_sprites.begin(); type != LibNcurses::_sprites.end(); type++)
-			for (dir = type->second.begin(); dir != type->second.end(); dir++)
-				if (((*obj)->getType() == type->first) && ((*obj)->getCurrentDirection().getCardinal() == dir->first))
-					this->_display_sprite((*obj)->getPosition().getX(), (*obj)->getPosition().getY(), dir->second, (*obj)->getType());
+	{
+		type = LibNcurses::_sprites.find((*obj)->getType());
+		if (type != LibNcurses::_sprites.end())
+		{
+			dir = type->second.find((*obj)->getCurrentDirection().getCardinal());
+			if (dir != type->second.end())
+				this->_display_sprite((*obj)->getPosition().getX(), (*obj)->getPosition().getY(), dir->second, (*obj)->getType());
+		}
+	}
+	this->_draw_outline();
 	wrefresh(this->_window);
 	return ;
 }
@@ -132,19 +108,21 @@ void				LibNcurses::display_score(std::list<int> const scores)
 {
 	int				max_x;
 	int				max_y;
-	std::list<int>::const_iterator		it;
+	int				p1;
+	int				p2;
 
-	it = scores.begin();
-	getmaxyx(stdscr, max_y, max_x);
-	wmove(this->_window, max_y, 1);
-	wprintw(this->_window, "P1:%d", *it);
-	wmove(this->_window, max_y, (max_x - 7));
-	wprintw(this->_window, "P2:%d", *(++it));
+	getmaxyx(this->_window, max_y, max_x);
+	p1 = scores.front();
+	p2 = scores.back();
+	wmove(this->_window, (max_y - 1), 1);
+	wprintw(this->_window, "P1:%d", p1);
+	wmove(this->_window, (max_y - 1), (max_x - (((std::to_string(p2)).length()) + 4)));
+	wprintw(this->_window, "P2:%d", p2);
 	wrefresh(this->_window);
 	return ;
 }
 
-void				LibNcurses::_display_sprite(int const x, int const y, char const sprite, type_e const type)
+void				LibNcurses::_display_sprite(int const x, int const y, std::string const sprite, type_e const type)
 {
 	int				pair;
 
@@ -159,14 +137,43 @@ void				LibNcurses::_display_sprite(int const x, int const y, char const sprite,
 	if ((type == SNAKE_HEAD_2) || (type == SNAKE_BODY_2) || (type == SNAKE_TAIL_2))
 		pair = 4;
 	wattron(this->_window, COLOR_PAIR(pair));
-	wprintw(this->_window, "%c", sprite);
+	wprintw(this->_window, "%s", sprite.c_str());
 	wattroff(this->_window, COLOR_PAIR(pair));
+	return ;
+}
+
+void				LibNcurses::_draw_outline(void)
+{
+	int				x;
+	int				y;
+
+	getmaxyx(this->_window, y , x);
+	wattron(this->_window, COLOR_PAIR(1));
+	wborder(this->_window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	wattroff(this->_window, COLOR_PAIR(1));
+	wmove(this->_window, 0, ((x / 2) - (this->_name.length() / 2)));
+	wprintw(this->_window, "%s", this->_name.c_str());
 	return ;
 }
 
 void				LibNcurses::_terminate(void)
 {
+	wclear(this->_window);
+	wrefresh(this->_window);
 	delwin(this->_window);
 	endwin();
 	return ;
+}
+
+extern "C"
+{
+	IGraphicLib			*getDynLibPointer(int const x, int const  y, std::string const winName)
+	{
+		return new LibNcurses(x, y, winName);
+	}
+
+	void				delLibPointer(IGraphicLib *lib_ptr)
+	{
+		delete lib_ptr;
+	}
 }
